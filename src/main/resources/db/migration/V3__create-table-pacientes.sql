2a570aac23eb28ee69f5c02dc77ab01e4581ceb4
edigilson.bento <edigilsonjunior@gmail.com>
2023-07-05 09:13:06 -0300

    ajustando para postgres

--- src/main/resources/db/migration/V3__create-table-pacientes.sql
+++ src/main/resources/db/migration/V3__create-table-pacientes.sql
@@ -1,5 +1,5 @@
 create table pacientes(
-    id bigint not null auto_increment,
+    id SERIAL,
     nome varchar(100) not null,
     email varchar(100) not null unique,
     cpf varchar(14) not null unique,
