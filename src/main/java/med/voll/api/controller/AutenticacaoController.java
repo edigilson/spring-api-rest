1f7da94436957956752469679b8f09925c5946b5
edigilson.bento <edigilsonjunior@gmail.com>
2023-07-05 09:14:31 -0300

    jwtToken

--- src/main/java/med/voll/api/controller/AutenticacaoController.java
+++ src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -2,6 +2,9 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.domain.user.DadosAutenticacao;
+import med.voll.api.domain.user.User;
+import med.voll.api.infra.security.DadosTokenJWT;
+import med.voll.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,11 +21,14 @@ public class AutenticacaoController {
     @Autowired
     private AuthenticationManager manager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity login(@RequestBody @Valid DadosAutenticacao dados){
-        var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
-        var authentyication =  manager.authenticate(token);
-
-        return  ResponseEntity.ok().build();
+        var authenticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        var authentyication =  manager.authenticate(authenticationToken);
+        var jwtToken = tokenService.gerarToken((User) authentyication.getPrincipal());
+        return  ResponseEntity.ok(new DadosTokenJWT(jwtToken));
     }
 }
