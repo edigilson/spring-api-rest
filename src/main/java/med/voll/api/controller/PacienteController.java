b4649cc8b3b85621e9e748dd80b8bee129ded77a
edigilson.bento <edigilsonjunior@gmail.com>
2023-07-07 08:16:00 -0300

    finalizando autenticação

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -19,6 +20,7 @@ public class PacienteController {
     private PacienteRepository pacienteRepository;
     @PostMapping
     @Transactional
+    @Secured("ROLE_ADMIN")
     public ResponseEntity<DadosDetalhamentoPaciente> cadastrar(@RequestBody @Valid DadosCadastroPaciente dados, UriComponentsBuilder uriComponentsBuilder){
         var paciente = new Paciente(dados);
         pacienteRepository.save(paciente);
--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -4,7 +4,10 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import med.voll.api.domain.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -16,12 +19,19 @@ public class SecurityFilter extends OncePerRequestFilter{
     @Autowired
     private TokenService tokenService;
 
+    @Autowired
+    UserRepository userRepository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var tokenJWT = recuperarToken(request);
+        if(tokenJWT != null) {
+            var subject = tokenService.getSubject(tokenJWT);
+            var usuario = userRepository.findByLogin(subject);
+            var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
 
-        var subject = tokenService.getSubject(tokenJWT);
-        System.out.println(subject);
+        }
 
         filterChain.doFilter(request, response);
 
@@ -29,10 +39,10 @@ public class SecurityFilter extends OncePerRequestFilter{
 
     private String recuperarToken(HttpServletRequest request) {
         var authorizationHeader = request.getHeader("Authorization");
-        if (authorizationHeader == null) {
-            throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization!");
+        if (authorizationHeader != null) {
+            return authorizationHeader.replace("Bearer ", "");
         }
+        return null;
 
-        return authorizationHeader.replace("Bearer ", "");
     }
 }
